766eb7c7b4e0694534644d635c527591088c1d4f
George Willcox <georgewillcox@outlook.com>
2022-05-31 20:20:57 +0100

    Sorted mobile nav bar

--- scripts/nav_bar.js
+++ scripts/nav_bar.js
@@ -1,31 +1,29 @@
-let top_bar = false;
-toggle_top_bar();
+window.dispatchEvent(new Event('scroll'));  // Just to make sure styling is correct if page is refreshed
 
 window.addEventListener('scroll', function () {
-    if (window.innerWidth < 1260 && top_bar) {
+    if (window.innerWidth < 1260) {
+        let navigation = document.getElementsByClassName('navigation')[0];
+        let elements = navigation.getElementsByTagName("li");
         if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-            document.getElementsByClassName("top-bar")[0].style.top = "0";
+            navigation.style.backgroundColor = '#444';
+            navigation.style.color = 'white';
+            navigation.style.boxShadow = '0 4px 12px 0 rgba(0, 0, 0, 0.3)';
+            Array.from(elements).forEach(element => {
+                element.style.padding = '20px 10px';
+                element.style.fontSize = '1.2rem';
+            });
         } else {
-            document.getElementsByClassName("top-bar")[0].style.top = "-50px";
+            navigation.style = null;
+            Array.from(elements).forEach(element => {
+                element.style = null;
+            });
         }
     }
 });
 
-function toggle_top_bar () {
-    if ((this.innerHeight <= 700 || this.innerWidth <= 1260)) {
-        if (!top_bar) {
-            Array.prototype.forEach.call(this.document.getElementsByClassName("navigation"), function (element) {
-                element.classList.add("top-bar");
-            });
-            top_bar = true;
-        }
-    } else {
-        Array.prototype.forEach.call(this.document.getElementsByClassName("navigation"), function (element) {
-            element.classList.remove("top-bar");
-            element.style.top = null;
-        });
-        top_bar = false;
+window.addEventListener('resize', function () {
+    if (window.innerWidth >= 1260) {
+        let navigation = document.getElementsByClassName('navigation')[0];
+        navigation.style = null;
     }
-}
-
-window.addEventListener('resize', toggle_top_bar);
+});
