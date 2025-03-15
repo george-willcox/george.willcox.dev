013a97b3ab8153cd543225f08ace661a28f4138e
George Willcox <georgewillcox@outlook.com>
2025-03-15 18:39:16 +0000

    Updated accoridon code to allow for dynamic content sizes

--- scripts/accordion.js
+++ scripts/accordion.js
@@ -4,6 +4,7 @@ for (var i = 0; i < accordion.length; i++) {
     accordion[i].addEventListener("click", function () {
         var collapse = this.parentElement.nextElementSibling;
         this.parentElement.classList.toggle("open");
+
         if (collapse.style.maxHeight) {
             collapse.style.transitionDuration = "0.25s";
             collapse.style.maxHeight = null;
@@ -12,4 +13,15 @@ for (var i = 0; i < accordion.length; i++) {
             collapse.style.maxHeight = collapse.scrollHeight + "px";
         }
     });
+
+    // Observe changes in size (including dynamic content like images)
+    var content = accordion[i].parentElement.nextElementSibling;
+    var resizeObserver = new ResizeObserver(() => {
+        if (content.style.maxHeight) {
+            content.style.transitionDuration = "0";
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
+
+    resizeObserver.observe(content);
 }
